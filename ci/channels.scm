1f7fbfb993b4e9ae0c62130d658c55ef7702a426
prop4n <legrandenzo.pro@gmail.com>
2026-04-20 18:44:38 +0200

    ci: add channels.scm with nonguix, restore guix pull

---
+++ ci/channels.scm
@@ -0,0 +1,8 @@
+(list (channel
+       (name 'guix)
+       (url "https://git.savannah.gnu.org/git/guix.git")
+       (branch "master"))
+      (channel
+       (name 'nonguix)
+       (url "https://gitlab.com/nonguix/nonguix")
+       (branch "master")))
